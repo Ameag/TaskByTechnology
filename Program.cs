5a62795893a47c958ccf3a4981b906e345bf1188
Kuchin Aleksey <nekqga@gmail.com>
2023-03-17 22:12:47 +0500

    Добавляет алгоритм для запуска задач

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello Maxim Technology");
+﻿using Tecjnology.tasks;
+
+const string taskPreviewTemplate = "Task number {0} is started";
+
+Console.WriteLine("Hello Maxim Technology");
+
+ITask[] tasks = { new Task1() };
+for(int i=0; i<tasks.Length; i++)
+{
+    var task = tasks[i];
+    Console.WriteLine(taskPreviewTemplate, i+1);
+    task.Execute();
+}
