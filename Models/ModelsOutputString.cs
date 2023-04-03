609f5c5991f13197573a139e72f76e8e23fe05fb
Kuchin Aleksey <nekqga@gmail.com>
2023-04-03 12:10:27 +0500

    Добавляет решение седьмой задачи

---
+++ Models/ModelsOutputString.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechnologyASP.Models
+{
+    public class ModelsOutputString
+    {
+        public string? Input { get; set; }
+        public string? Result { get; set; }
+        public Dictionary<char, int>? SymbolCount { get; set; }
+        public string? FinalSubstring { get; set; }
+        public string? SortedString { get; set; }
+        public string? RemovedString { get; set; }
+        public HashSet<char>? BadSymbol { get; set; }
+    }
+
+}
